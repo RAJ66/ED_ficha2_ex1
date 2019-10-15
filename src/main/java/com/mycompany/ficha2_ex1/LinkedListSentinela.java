96ebcff83d9da3cb85992d717065e4c0a7a8e3bb
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-10-15 18:16:35 +0100

    classe LinkedListSentilena documentada

--- src/main/java/com/mycompany/ficha2_ex1/LinkedListSentinela.java
+++ src/main/java/com/mycompany/ficha2_ex1/LinkedListSentinela.java
@@ -11,68 +11,84 @@ package com.mycompany.ficha2_ex1;
  */
 public class LinkedListSentinela<T> {
 
-    private int count;
+    /**
+     * count - variável com o número de elementos na lista head - Node que
+     * aponta para o head da lista tail - Node que aponta para a tail da lista
+     */
     private Node<T> head;
     private Node<T> tail;
+    private int count;
 
+    /**
+     * Método construtor que instancia o node head e tail a null e inicializa
+     * count a 0 Coloca o Node tail como next de Head
+     */
     public LinkedListSentinela() {
+        this.head = new Node(null);
+        this.tail = new Node(null);
         this.count = 0;
-        this.head = new Node<>(null);
-        this.tail = new Node<>(null);
-
         this.head.setNext(this.tail);
     }
 
+    /**
+     *
+     * @return número de elementos da lista
+     */
     public int getCount() {
         return count;
     }
 
-
-
-    public void add(T novo) {
-        Node<T> newNode = new Node<T>(novo);
+    /**
+     * Método que adiciona um elemento na lista
+     *
+     * @param element - elemento a adicionar na lista
+     */
+    public void add(T element) {
+        Node<T> newNode = new Node<>(element);
 
         newNode.setNext(this.head.getNext());
         this.head.setNext(newNode);
         this.count++;
     }
 
-    public boolean remove(T novo) {
+    /**
+     * Método que remove um elemento da lista
+     *
+     * @param element - elemento a remover
+     * @return valor booleano conforme o sucesso ou insucesso do método
+     */
+    public boolean remove(T element) {
         boolean found = false;
+        Node<T> current = this.head.getNext();
         Node<T> previous = this.head;
-        Node<T> current =this.head.getNext();
 
-        
-
-        while (current.getElement() != null ) {
-            if (current.getElement().equals(novo)) {
+        while (current.getElement() != null) {
+            if (current.getElement().equals(element)) {
                 previous.setNext(current.getNext());
                 this.count--;
-                return true;
-
+                return found;
             } else {
                 previous = current;
                 current = current.getNext();
             }
         }
-        
-        return false;
-        
-        
 
+        return found;
     }
 
+    /**
+     * Método que imprime todos os elementos da lista
+     */
     public void print() {
-         Node<T> current = this.head.getNext();
+        Node<T> current = this.head.getNext();
 
         while (current != this.tail) {
-
             System.out.println(current.getElement().toString());
-
             current = current.getNext();
         }
 
-        System.out.println(this.count);
+        System.out.println("Número de elementos na lista: " + this.count);
     }
+    
 
 }
